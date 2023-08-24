f775e33d865a7fc4ef14667aca30276fe6d5792a
Riyad <Riyad9876@gmail.com>
2023-08-24 12:46:54 -0400

    greeting param

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.get('/greeting', (req, res)=>{
 
 
 
+app.get('/greeting/:name', (req, res)=>{
+    console.log(req.params);
+    const name = req.params.name
+    const capital = name[0].toUpperCase() + name.slice(1)
+    res.send(`${capital}! It's so great to see you!`)
+})
+
+
+
+
+
+
+
+
 
 
 
