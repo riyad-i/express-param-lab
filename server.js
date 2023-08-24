37c93b7b9d51d04a728cfb003f7c2e0c65cd1b55
Riyad <Riyad9876@gmail.com>
2023-08-24 14:44:22 -0400

    8ball

--- server.js
+++ server.js
@@ -32,8 +32,16 @@ app.get('/tip/:total/:tipPercentage', (req, res)=>{
 })
 
 
+const magicResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
+app.get('/magic/:question', (req, res)=>{
+    console.log(req.params);
 
+    const random = Math.floor(Math.random() * (magicResponses.length))
+    
+    res.send(req.params.question + ' : ' + magicResponses[random])
+
+})
 
 
 
