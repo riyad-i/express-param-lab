ea1d080842b47a18f5de7fafeaaa2410c9c6b039
Riyad <Riyad9876@gmail.com>
2023-08-24 13:04:14 -0400

    tip route calculator

--- server.js
+++ server.js
@@ -15,7 +15,20 @@ app.get('/greeting/:name', (req, res)=>{
     console.log(req.params);
     const name = req.params.name
     const capital = name[0].toUpperCase() + name.slice(1)
-    res.send(`${capital}! It's so great to see you!`)
+    res.send(`<h1>${capital}! It's so great to see you!</h1>`)
+})
+
+
+
+
+
+
+app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    console.log(req.params);
+    const total = req.params.total
+    const tipPercentage = req.params.tipPercentage
+    const tipAmount = total*(tipPercentage/100)
+    res.send(`<h1> Your tip should be $${tipAmount} on a $${total} bill with a tip percentage of ${tipPercentage}%</h1>`)
 })
 
 
